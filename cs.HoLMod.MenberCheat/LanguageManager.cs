7e319d9ec2bcb2556f8ad70aecfe4acdce300503
AnZhi20 <605997266@qq.com>
2025-10-14 13:52:45 +0800

    Add Plan ：LanguageManager

---
+++ cs.HoLMod.MenberCheat/LanguageManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs.HoLMod.MemberCheat
+{
+    internal class LanguageManager
+    {
+    }
+}
