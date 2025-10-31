7fbbc2de34f6b3a67a04dad6f3960069c4f0a366
AnZhi20 <605997266@qq.com>
2025-11-01 06:15:23 +0800

    新建项目-事件触发器

---
+++ cs.HoLMod.AddEvents/Main.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs.HoLMod.AddEvents
+{
+    // 插件信息类
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "cs.HoLMod.AddEvents.AnZhi20";
+        public const string PLUGIN_NAME = "HoLMod.AddEvents";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class AddEvents : BaseUnityPlugin
+    {
+        // 
+    }
+}
