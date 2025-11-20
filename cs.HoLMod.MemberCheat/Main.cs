bc190367eabbccbde9ec28bb603c6affe9527173
AnZhi20 <605997266@qq.com>
2025-11-20 20:09:26 +0800

    添加AB包加载逻辑和对象挂载逻辑

--- cs.HoLMod.MemberCheat/Main.cs
+++ cs.HoLMod.MemberCheat/Main.cs
@@ -24,8 +24,87 @@ namespace cs.HoLMod.MemberCheat
     }
 
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class MenberCheat : BaseUnityPlugin
+    public class MemberCheat : BaseUnityPlugin
     {
-        // 加载ab包
+        public static AssetBundle memberCheatBundle;
+        public static GameObject memberCheatObject;
+        
+        private void Awake()
+        {
+            LoadAssetBundle();
+        }
+        
+        private void LoadAssetBundle()
+        {
+            try
+            {
+                // 加载ab包
+                string bundlePath = Path.Combine(Paths.PluginPath, "membercheat" + ".bundle");
+                if (File.Exists(bundlePath))
+                {
+                    memberCheatBundle = AssetBundle.LoadFromFile(bundlePath);
+                    if (memberCheatBundle != null)
+                    {
+                        Logger.LogInfo("成功加载membercheat包");
+                        // 从ab包中加载membercheat对象
+                        memberCheatObject = memberCheatBundle.LoadAsset<GameObject>("membercheat");
+                        if (memberCheatObject != null)
+                        {
+                            Logger.LogInfo("成功从ab包中加载membercheat对象");
+                            // 实例化对象
+                            memberCheatObject = GameObject.Instantiate(memberCheatObject);
+                            GameObject.DontDestroyOnLoad(memberCheatObject);
+                            
+                            // 挂载Panel_Main.cs到membercheat对象
+                            if (!memberCheatObject.GetComponent<membercheat>())
+                            {
+                                memberCheatObject.AddComponent<membercheat>();
+                                Logger.LogInfo("成功挂载Panel_Main.cs到membercheat对象");
+                            }
+                            
+                            // 挂载PanelA.cs到membercheat子对象PanelA
+                            Transform panelATransform = memberCheatObject.transform.Find("PanelA");
+                            if (panelATransform != null)
+                            {
+                                if (!panelATransform.gameObject.GetComponent<MemberCheatPanelA>())
+                                {
+                                    panelATransform.gameObject.AddComponent<MemberCheatPanelA>();
+                                    Logger.LogInfo("成功挂载PanelA.cs到membercheat子对象PanelA");
+                                }
+                            }
+                            else
+                            {
+                                Logger.LogError("未找到membercheat对象的子对象PanelA");
+                            }
+                        }
+                        else
+                        {
+                            Logger.LogError("无法从ab包中加载membercheat对象");
+                        }
+                    }
+                    else
+                    {
+                        Logger.LogError("无法加载membercheat包");
+                    }
+                }
+                else
+                {
+                    Logger.LogError("membercheat包文件不存在: " + bundlePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("加载ab包时发生错误: " + ex.Message);
+            }
+        }
+        
+        private void OnDestroy()
+        {
+            // 卸载ab包
+            if (memberCheatBundle != null)
+            {
+                memberCheatBundle.Unload(false);
+            }
+        }
     }
 }
